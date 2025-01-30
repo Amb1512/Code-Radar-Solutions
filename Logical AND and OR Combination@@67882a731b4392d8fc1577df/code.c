b237c6a83d240e2cae0397032f6ce99daab0ef52
Amb1512 <195753074+Amb1512@users.noreply.github.com>
2025-01-30 17:52:20 +0530

    Solution to Logical AND and OR Combination

--- Logical AND and OR Combination@@67882a731b4392d8fc1577df/code.c
+++ Logical AND and OR Combination@@67882a731b4392d8fc1577df/code.c
@@ -9,10 +9,10 @@ void main()
     else if (x==0 || y==0)
         printf("True");
     else if(x>=0 && y<=0){
-        printf("True")
+        printf("True");
     }
     else if(x>=0 || y<=0){
-        printf("True")
+        printf("True");
     }
     else
         printf("False");
