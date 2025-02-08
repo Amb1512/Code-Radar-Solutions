c8a1113b6841fcdd1774c08b856636687773a87a
Amb1512 <195753074+Amb1512@users.noreply.github.com>
2025-02-08 18:26:21 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,9 +4,11 @@ void main()
     int a;
     int b;
     char c;
+    int sum;
     scanf("%d%d%c",&a,&b,&c);
+    sum=a+b;
     if (c=='+')
-        printf("%d",a+b);
+        printf("%d",sum);
     else if (c=='-')
         printf("%d",a-b);
 }
